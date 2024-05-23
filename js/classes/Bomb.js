1d7ff23a55c0195147c98d4abbf4f40048607bdf
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-23 11:32:52 -0400

    add update function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -73,4 +73,10 @@ class PowerUp {
         c.fill();
         c.closePath();
     }
+
+    update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw();
+ }
 }
