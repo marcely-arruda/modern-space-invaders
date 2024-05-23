b8636e4e9245acc814c5ef2e2ed662933ada5400
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-23 10:26:52 -0400

    add draw function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -11,5 +11,16 @@ class Bomb {
         gsap.to(this, {
             radius: 30
         });
+    }""
+
+    draw() {
+        c.save();
+        c.globalAlpha = this.opacity;
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.closePath();
+        c.fillStyle = this.color;
+        c.fill();
+        c.restore();
     }
 }
