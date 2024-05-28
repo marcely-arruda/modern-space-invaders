e6a60b15ba449a5347ed141e836f6ededf0b1dd2
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 10:08:39 -0400

    add update function in Grid class

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -30,4 +30,13 @@ class Grid {
       }
     }
   }
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
+    if(this.position.x + this.width >= canvas.width || this.position.x < 0) {
+      this.velocity.x = -this.velocity.x * 1.15;
+      this.velocity.y = 30;
+    }
+  }
 }
