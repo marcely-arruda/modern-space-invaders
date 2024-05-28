545a53621f035db182ddfb9f10a3d8e69bcd6b42
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 11:49:07 -0400

    add draw function in Particle class

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -7,4 +7,15 @@ class Particle {
         this.fades = fades;
         this.opacity = 1;
     }
+
+    draw() {
+        c.save();
+        c.globalAlpha = this.opacity;
+        c.fillStyle = this.color;
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
 }
