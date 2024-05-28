a949a4d037d2047731d529e97226f33f55e449d1
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 10:22:19 -0400

    add Invader class

---
+++ js/classes/Invader.js
@@ -0,0 +1,22 @@
+class Invader {
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      X: 0
+    };
+
+    const image = new Image();
+    image.src = "./img/invader.png";
+
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: position.x,
+        y: position.y
+      };
+    };
+  }
+}
