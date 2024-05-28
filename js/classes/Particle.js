a398f16f38442dfe82a575f6b7930b029fbe8ec1
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 11:44:18 -0400

    add Particle class

---
+++ js/classes/Particle.js
@@ -0,0 +1,10 @@
+class Particle {
+    constructor({position, velocity, radius, color, fades}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+        this.color = color;
+        this.fades = fades;
+        this.opacity = 1;
+    }
+}
