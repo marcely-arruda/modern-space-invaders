a1539147b370b68b4f873718ffa70db9841b7782
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 11:23:57 -0400

    add Projectile class

---
+++ js/classes/Projectile.js
@@ -0,0 +1,8 @@
+class projectile {
+    constructor({position, velocity, color = "red"}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 4;
+        this.color = color;
+    }
+}
