b586e21c51131d19676433c2c5e156e08432a4b6
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 11:51:52 -0400

    add update function in Particle class

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -1,21 +1,28 @@
 class Particle {
-    constructor({position, velocity, radius, color, fades}) {
-        this.position = position;
-        this.velocity = velocity;
-        this.radius = radius;
-        this.color = color;
-        this.fades = fades;
-        this.opacity = 1;
-    }
+  constructor({ position, velocity, radius, color, fades }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = radius;
+    this.color = color;
+    this.fades = fades;
+    this.opacity = 1;
+  }
 
-    draw() {
-        c.save();
-        c.globalAlpha = this.opacity;
-        c.fillStyle = this.color;
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        c.fill();
-        c.closePath();
-        c.restore();
-    }
+  draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.fillStyle = this.color;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fill();
+    c.closePath();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.fades) this.opacity -= 0.01;
+  }
 }
