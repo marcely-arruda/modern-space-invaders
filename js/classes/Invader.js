07190d9d94af444b0e78104e580dfbb8cf7a2454
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-28 11:07:39 -0400

    add shoot function in Invader class

--- js/classes/Invader.js
+++ js/classes/Invader.js
@@ -21,20 +21,36 @@ class Invader {
   }
 
   draw() {
-    c.drawImage( 
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
     );
   }
 
-  update({velocity}) {
-    if(this.image) {
-        this.draw();
-        this.position.x += velocity.x;
-        this.position.y += velocity.y;
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
     }
+  }
+
+  shoot(invaderProjectiles) {
+    audio.enemyShoot.play();
+    invaderProjectiles.push(
+      new invaderProjectile({
+        position: {
+          x: this.position.x + this.width / 2,
+          y: this.position.y + this.height 
+        },
+        velocity: {
+          x: 0,
+          y: 5
+        }
+      })
+    );
   }
 }
