016121ff5d133dc3e6fbb275edf4995c72e978d4
marcely-arruda <arrudamarcely460@gmail.com>
2024-06-06 11:42:23 -0400

    add draw rectangularCollision function in util.js file

--- js/util.js
+++ js/util.js
@@ -21,4 +21,12 @@ function createScoreLabel({ score = 100, object }) {
       document.querySelector("#parentDiv").removeChild(scoreLabel);
     }
   });
+
+  function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y && 
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+    );   
+  }
 }
