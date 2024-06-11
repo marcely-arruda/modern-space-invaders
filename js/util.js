e658baf8acb172c9040abdd7e4f4d56d65f83b08
marcely-arruda <arrudamarcely460@gmail.com>
2024-06-11 09:52:02 -0400

    add creatParticles function in util.js file

--- js/util.js
+++ js/util.js
@@ -21,12 +21,32 @@ function createScoreLabel({ score = 100, object }) {
       document.querySelector("#parentDiv").removeChild(scoreLabel);
     }
   });
+}
 
-  function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.position.y + rectangle1.height >= rectangle2.position.y && 
-        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
-        rectangle1.position.x <= rectangle2.position.x + rectangle2.width
-    );   
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+  );
+}
+
+function createParticles({ object, color, fades }) {
+  for (let i = 0; i < 15; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: object.position.x + object.width / 2,
+          y: object.position.y + object.height / 2
+        },
+        velocity: {
+          x: (Math.random() - 0.5) * 2,
+          y: (Math.random() - 0.5) * 2
+        },
+        radius: Math.random() * 3,
+        color: color || "#DAA0DE",
+        fades
+      })
+    );
   }
 }
