3f147196d2deff41946c2a23ea6cc84860d467a0
marcely-arruda <arrudamarcely460@gmail.com>
2024-06-13 08:28:06 -0400

    Initialized the creation of the animate function

--- index.js
+++ index.js
@@ -109,3 +109,19 @@ function endGame() {
     fades: true
   });
 }
+
+function animate() {
+    if (!game.active) return;
+    requestAnimationFrame(animate);
+
+    const msNow = window.performance.now();
+    const elapsed = msNow - msPrev;
+
+    if(elapsed <  fpsInterval) return;
+
+    msPrev = msNow - (elapsed % fpsInterval);
+
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+}
