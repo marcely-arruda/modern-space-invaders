1b5e3d86056aaaa9fd70eb49e9861f4c7b740054
marcely-arruda <arrudamarcely460@gmail.com>
2024-06-13 07:52:35 -0400

    add init function in index.js file

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ let bombs = [];
 let powerUps = [];
 
 let keys = {
-    ArrowLeft: {
-        pressed: false
-    },
-    ArrowRight: {
-        pressed: false
-    },
-    space: {
-        pressed: false
-    }
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
 };
 
 let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
 
 let game = {
-    over: false,
-    active: true
+  over: false,
+  active: true
 };
 
 let score = 0;
@@ -40,3 +40,52 @@ let fps = 60;
 let fpsInterval = 1000 / fps;
 
 let msPrev = window.performance.now();
+
+function init() {
+  player = new Player();
+  projectiles = [];
+  grids = [];
+  invaderProjectiles = [];
+  particles = [];
+  bombs = [];
+  powerUps = [];
+
+  keys = {
+    ArrowLeft: {
+      pressed: false
+    },
+    ArrowRight: {
+      pressed: false
+    },
+    space: {
+      pressed: false
+    }
+  };
+
+  frames = 0;
+  randomInterval = Math.floor(Math.random() * 500 + 500);
+
+  game = {
+    over: false,
+    active: true
+  };
+
+  score = 0;
+
+  for (let i = 0; i < 100; i++) {
+    particles.push(
+      new particle({
+        position: {
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height
+        },
+        velocity: {
+          x: 0,
+          y: 0.3
+        },
+        radius: Math.random() * 2,
+        color: "white"
+      })
+    );
+  }
+}
