ce838de79d65dc1e5e0032555ed77f5f7d94757d
marcely-arruda <arrudamarcely460@gmail.com>
2024-06-20 08:09:37 -0400

    finished animate function and added event to start the game

--- index.js
+++ index.js
@@ -379,4 +379,52 @@ function animate() {
     player.velocity.x = 0;
     player.rotation = 0;
   }
+
+  if(frames % randomInterval === 0) {
+    spawnBuffer = spawnBuffer < 0 ? 100 : spawnBuffer;
+    grid.push(new Grid());
+    randomInterval = Math.floor(Math.random() * 500 + spawnBuffer);
+    frames = 0;
+    spawnBuffer -= 100;
+  }
+
+  if (
+    keys.space.pressed && 
+    player.powerUp === "Metralhadora" &&
+    frames % 2 === 0 &&
+    !game.over
+  ) {
+    if(frames % 6 === 0) audio.shoot.play();
+    projectiles.push(
+      new projectile ({
+        position: {
+          x: player.position.x + player.width / 2,
+          y: player.position.y
+        },
+        velocity: {
+          x: 0,
+          y: -10
+        },
+        color: "yellow"
+      })
+    );
+  }
+  frames ++;
 }
+
+document.querySelector(`#startButton`).addEventListener("click", () => {
+  audio.backgroundMusic.play();
+  audio.start.play();
+
+  document.querySelector("#startScreen").style.display = "none";
+  document.querySelector("#scoreContainer").style.display = "block";
+  init();
+  animate();
+});
+
+document.querySelector("#restartScreen").addEventListener("click", () => {
+  audio.select.play();
+  document.querySelector("#restartScreen").style.display = "none";
+  init();
+  animate();
+});
